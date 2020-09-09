b9f3136d9a843c569289b9cba48b902998c6560a
Quan666 <Quan666@users.noreply.github.com>
2020-09-09 23:48:56 +0800

    fix : ip

--- Server.py
+++ Server.py
@@ -16,7 +16,7 @@ async def hello(websocket, path):
     await websocket.send(r.content)
     
 
-start_server = websockets.serve(hello, 'localhost', 8765)
+start_server = websockets.serve(hello, '0.0.0.0', 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
